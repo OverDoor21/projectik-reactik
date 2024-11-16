35cc42a2b16455093b4725fabc509f49e285ae2c
OverDoor21 <aulin.ivan32@gmail.com>
2024-11-16 16:14:10 +0100

    Init

---
+++ src/app/text.ts
@@ -0,0 +1,7 @@
+// Declare the variable and initialize it (optional)
+export let TextUserInput: string = "";
+
+// You could also create a setter function if needed
+export function setTextUserInput(value: string): void {
+  TextUserInput = value;
+}
